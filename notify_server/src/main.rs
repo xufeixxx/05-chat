bae7e7559fc570fcd9b726c7795bb40b6273b42d
xufei <xufei@xufei-VMware-Ubuntu24.04LTS>
2024-06-05 21:44:10 +0800

    feature: refactor code to a workspace and start notify server

--- notify_server/src/main.rs
+++ notify_server/src/main.rs
@@ -1,4 +1,4 @@
-use chat::{get_router, AppConfig};
+use notify_server::get_router;
 use tokio::net::TcpListener;
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
@@ -8,11 +8,11 @@ async fn main() -> anyhow::Result<()> {
     let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
-    let config = AppConfig::load()?;
+    // let config = AppConfig::load()?;
     // let port = config.server.port;
-    let addr = format!("0.0.0.0:{}", config.server.port);
+    let addr = "0.0.0.0:6687";
 
-    let app = get_router(config);
+    let app = get_router();
     let listener = TcpListener::bind(&addr).await?;
     info!("Listening on: {}", addr);
 
